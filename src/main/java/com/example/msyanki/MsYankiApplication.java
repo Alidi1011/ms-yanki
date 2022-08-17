a000a5d64c859958789ff227ff9f01260454ab8e
Alisson <arteaga10alisson@gmail.com>
2022-08-17 15:42:30 -0500

    register in eureka server

--- src/main/java/com/example/msyanki/MsYankiApplication.java
+++ src/main/java/com/example/msyanki/MsYankiApplication.java
@@ -3,6 +3,9 @@ package com.example.msyanki;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
 @SpringBootApplication
 @Slf4j
 public class MsYankiApplication {
